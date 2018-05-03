d5797608fe954a8e35874c99dae73b859d7383de
Judy McGoldrick <jjeanius@verizon.net>
2018-05-03 02:58:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,7 +62,7 @@ class Student
       sql = <<-SQL
         SELECT * FROM students
         WHERE students.grade = 10
-        LIMIT 1
+        LIMIT 2
       SQL
 
       DB[:conn].execute(sql).collect do |row|
