38308456458e8be51895c6456b9174fef31d5228
Judy McGoldrick <jjeanius@verizon.net>
2018-05-03 03:22:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,9 +88,9 @@ class Student
         SELECT * FROM students
         WHERE students.grade = ?
         SQL
-
-        DB[:conn].execute(sql).collect do |row|
-          self.new_from_db(row)
+        DB[:conn].execute(sql, student)
+      #  DB[:conn].execute(sql).collect do |row|
+      #    self.new_from_db(row)
       end
     end
 
